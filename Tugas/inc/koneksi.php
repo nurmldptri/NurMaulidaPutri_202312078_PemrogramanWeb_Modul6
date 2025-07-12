687093a39616d99854dec4c9151484bba4e72afd
Nur Maulida Putri <nurrmaulidaaputrii@gmail.com>
2025-07-12 19:05:58 +0800

    Create koneksi.php

---
+++ Tugas/inc/koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'db_toko';
+$user = 'root';
+$pass = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+?>
